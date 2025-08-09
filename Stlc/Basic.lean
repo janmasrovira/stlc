bcbc2cd0c3072ec0d68e2494549acde1eca7a8eb
Jan Mas Rovira <janmasrovira@gmail.com>
2025-08-10 01:39:12 +0200

    explore

--- Stlc/Basic.lean
+++ Stlc/Basic.lean
@@ -43,7 +43,7 @@ structure Var {n : Nat} (Γ : Context n) (ty : Ty) : Type where
 inductive Expr : {n : Nat} → (Γ : Context n) → Ty → Type where
   | lam
     {Γ : Context n}
-    (varTy : Ty)
+    {varTy : Ty}
     {bodyTy : Ty}
     (body : Expr (varTy ▹ Γ) bodyTy)
     : Expr Γ (varTy ⟶ bodyTy)
@@ -65,37 +65,28 @@ inductive Expr : {n : Nat} → (Γ : Context n) → Ty → Type where
     {Γ : Context n}
     (num : Expr Γ .Nat)
     : Expr Γ .Nat
-  | prec
-    {Γ : Context n}
-    {ty : Ty}
-    (z : Expr Γ ty)
-    (s : Expr Γ (.Nat ⟶ ty ⟶ ty))
-    (m : Expr Γ .Nat)
-    : Expr Γ ty
+  -- | prec
+  --   {Γ : Context n}
+  --   {ty : Ty}
+  --   (z : Expr Γ ty)
+  --   (s : Expr Γ (.Nat ⟶ ty ⟶ ty))
+  --   (m : Expr Γ .Nat)
+  --   : Expr Γ ty
 
 mutual
-
-inductive Closure : (varTy : Ty) → (retTy : Ty) → Type where
-  | mk
-    {n : Nat}
-    {Γ : Context n}
-    (env : Env Γ)
-    {varTy retTy : Ty}
-    (body : Expr (varTy ▹ Γ) retTy)
-    : Closure varTy retTy
 
 inductive Val : Ty → Type where
   | nat : Nat → Val .Nat
   | closure
-    {varTy : Ty}
-    {retTy : Ty}
-    (cl : Closure varTy retTy)
+    {Γ : Context n}
+    (env : Env Γ)
+    {varTy retTy : Ty}
+    (body : Expr (varTy ▹ Γ) retTy)
     : Val (varTy ⟶ retTy)
 
-inductive Env : {n : Nat} → (Γ : Context n) → Type where
+inductive Env : (Γ : Context n) → Type where
   | nil : Env .nil
   | cons
-    {n : Nat}
     {Γ : Context n}
     {ty : Ty}
     (val : Val ty)
@@ -131,31 +122,12 @@ def eval
   | .zero => .nat 0
   | .suc e => let .nat m := eval env e
               .nat (m + 1)
-  | @Expr.lam _ Γ ty bodyTy body => .closure ⟨env, body⟩
+  | .lam body => .closure env body
   | .var v => env.get v
-  | .app fn x => evalApp env fn x
-  | .prec z i n =>
-    let z' : Val ty := eval env z
-    let i' : Val (Ty.Nat ⟶ ty ⟶ ty) := eval env i
-    let nat : Val Ty.Nat := eval env n
-    let indCase (prevNum : Nat) (prevVal : Val ty) : Val ty :=
-                evalVApp (evalVApp i' (.nat prevNum)) prevVal
-    @Nat.rec (fun _ => Val ty) z' indCase nat.interpNat
-  where
-  evalApp
-    {n : Nat}
-    {Γ : Context n}
-    {ty1 ty2 : Ty}
-    (env : Env Γ)
-    (fn : Expr Γ (ty1 ⟶ ty2))
-    (arg : Expr Γ ty1)
-    : Val ty2 := evalVApp (eval env fn) (eval env arg)
+  | .app (l := l) (r := r) (fn : Expr Γ (l ⟶ r)) arg =>
+      let arg' := eval env arg
+      let .closure clEnv body := eval env fn
+      eval (.cons arg' clEnv) body
 
-  evalVApp
-    {ty1 ty2 : Ty}
-    (fn : Val (ty1 ⟶ ty2))
-    (arg : Val ty1)
-    : Val ty2 :=
-      let .closure c := fn
-      let @Closure.mk _ _ clEnv _ _ body := c
-      eval (.cons arg clEnv) body
+-- val < max expr env.succ
+-- env ≤ expr
